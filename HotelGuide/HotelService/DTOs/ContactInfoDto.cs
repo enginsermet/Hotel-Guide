81c3ca6debf83cdad2340a4b7b9f7118191a89f2
Engin <sermetengin12@gmail.com>
2024-01-20 16:42:24 +0300

    DTOs
    HotelDto, ContactInfoDto, CreateHotelDto, HotelDeatilsDto added

---
+++ HotelGuide/HotelService/DTOs/ContactInfoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelService.DTOs
+{
+    public class ContactInfoDto
+    {
+        public string? PhoneNumber { get; set; }
+        public string? MailAddress { get; set; }
+        public string? Location { get; set; }
+    }
+}
