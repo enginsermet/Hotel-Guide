cbd1d144f0a44814ccfc816d0565c7702000fde5
Engin <sermetengin12@gmail.com>
2024-01-20 16:44:36 +0300

    AutoMapper Mapping
    GeneralMapping added

---
+++ HotelGuide/HotelService/Mapping/GeneralMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HotelService.DTOs;
+using HotelService.Entities;
+
+namespace HotelService.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Hotel, HotelDto>().ReverseMap();
+            CreateMap<Hotel, CreateHotelDto>().ReverseMap();
+            CreateMap<Hotel, HotelDetailsDto>().ReverseMap();
+
+            CreateMap<ContactInfo, ContactInfoDto>().ReverseMap();
+            CreateMap<ContactInfo, CreateHotelDto>().ReverseMap();
+        }
+    }
+}
--- HotelGuide/HotelService/obj/Debug/net7.0/HotelService.AssemblyInfo.cs
+++ HotelGuide/HotelService/obj/Debug/net7.0/HotelService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d95cf075a02b63497f351e9293cfbaf6c22a3a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c3ca6debf83cdad2340a4b7b9f7118191a89f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
