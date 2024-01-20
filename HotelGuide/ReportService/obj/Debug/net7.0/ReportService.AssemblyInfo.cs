98d122e34e547d9ec4529b98972d2bc7b1c6621f
Engin <sermetengin12@gmail.com>
2024-01-20 21:49:29 +0300

    Update

--- HotelGuide/ReportService/obj/Debug/net7.0/ReportService.AssemblyInfo.cs
+++ HotelGuide/ReportService/obj/Debug/net7.0/ReportService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add77799ae7d80ecc067f08b422bfa92487fde5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540a21da27162096b65618acaa626ae94710632e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
